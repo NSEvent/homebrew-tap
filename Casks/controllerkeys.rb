19939431dc2e900ce7a632c73af55b9659bf8da0
NSEvent <kvntng@gmail.com>
2026-08-15 21:40:05 -0700

    🔮 controllerkeys 2.6.5

--- Casks/controllerkeys.rb
+++ Casks/controllerkeys.rb
@@ -1,6 +1,6 @@
 cask "controllerkeys" do
-  version "2.6.4"
-  sha256 "8b12dcd9d444123777d9fdfc5ac08c4e9c8d882a610797a21de4e7e515a21ae9"
+  version "2.6.5"
+  sha256 "4e8e3e8294c5489559635807314436eb5f64c81e00389e29e27163b3b2d66bb1"
 
   url "https://github.com/NSEvent/xbox-controller-mapper/releases/download/v#{version}/ControllerKeys-#{version}.dmg",
       verified: "github.com/NSEvent/xbox-controller-mapper/"
